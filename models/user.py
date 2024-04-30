2653dc1b92397404bf460b2ca75dc68c08f1f8a7
Lain0x0 <lainyamato03@gmail.com>
2024-04-30 19:38:24 +0100

    user_0x

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
