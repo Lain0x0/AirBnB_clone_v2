4254a406119ebe45aa8b91b911e37d426634b488
Lain0x0 <lainyamato03@gmail.com>
2024-04-30 19:34:47 +0100

    user_0x

--- models/user.py
+++ models/user.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+""" Setuping modules"""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database."""
-
+    """Inheriting from Base """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
