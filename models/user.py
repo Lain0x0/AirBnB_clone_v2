6f169a8d9451a0e692a75d3f97b8d57dba0c7488
Lain0x0 <lainyamato03@gmail.com>
2024-04-30 19:58:32 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Setuping modules"""
+"""Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -8,7 +8,8 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Inheriting from Base """
+    """Represents a user for a MySQL database."""
+
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
