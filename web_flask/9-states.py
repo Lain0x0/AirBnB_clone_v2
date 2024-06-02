4ccc7d946f72bc18feeb206b7667da3ecf42054e
Lain0x0 <lainyamato03@gmail.com>
2024-06-02 15:54:21 +0100

    Web Flask States-0x10

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all States"""
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown_app(exc):
+    """Remove SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
